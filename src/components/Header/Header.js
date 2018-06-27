4a00af3f39edc7cf2150569650c7b6476695042f
Rabah Zeineddine <rabah.zeineddine@hotmail.com>
2018-06-27 18:46:33 -0300

    feat: add header component

---
+++ src/components/Header/Header.js
@@ -0,0 +1,17 @@
+import React, {Component } from 'react'
+import './Header.css'
+
+class Header extends Component {
+
+
+
+    render(){
+        return (
+            <div className="header">
+                <h2 className="header-title">MyReads</h2>
+            </div>
+        )
+    }
+
+}
+export default  Header
