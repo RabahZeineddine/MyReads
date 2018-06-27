3ad2c53608041362b9d8e282dcdf166651acbcba
Rabah Zeineddine <rabah.zeineddine@hotmail.com>
2018-06-27 18:47:04 -0300

    feat: move app.js to components folder

---
+++ src/components/App.js
@@ -0,0 +1,30 @@
+import React, { Component } from 'react';
+import Header from './Header'
+import ListBooks from './ListBooks'
+import * as BooksAPI from '../utils/BooksAPI'
+import './App.css';
+
+class App extends Component {
+
+    state = {
+        books: []
+    }
+
+    componentDidMount(){
+        BooksAPI.getAll().then( books => {
+            this.setState({books})
+        })
+    }
+
+    render() {
+        return (
+            <div className="App">
+                <Header />
+                <ListBooks />
+            </div>
+        );
+    }
+
+}
+
+export default App;
