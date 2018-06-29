db505157b33fd52be314e646babb0e57244c47a6
Rabah Zeineddine <rabah.zeineddine@hotmail.com>
2018-06-28 22:26:34 -0300

    feat: add header img and navbar

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -3,12 +3,26 @@ import './Header.css'
 
 class Header extends Component {
 
-
-
     render(){
         return (
             <div className="header">
-                <h2 className="header-title">MyReads</h2>
+                <div className="header-top">
+                    <h2 className="header-title">MyReads</h2>
+                </div>
+                <div className="header-navbar">
+                    <div className="navbar-line"></div>
+                    <div className="navbar-content">
+                        <div className="navbar-left">
+                            <div className="navbar-item">Home</div>
+                            <div className="navbar-item">Find books</div>
+                        </div>
+                        <div className="navbar-right">
+                            <div className="navbar-item">Login</div>
+                            <div className="navbar-item">Signup</div>
+                        </div>
+                    </div>
+                    <div className="navbar-line"></div>
+                </div>
             </div>
         )
     }
