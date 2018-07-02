32e611b08a06ca3c18bd6d996e971bac9cd48532
Rabah Zeineddine <rabah.zeineddine@hotmail.com>
2018-07-02 15:53:49 -0300

    feat: add footer

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -6,7 +6,44 @@ class Footer extends Component{
 
     render(){
         return (
-            <div className="footer">Footer</div>
+            <div className="footer">
+                <div className="footer-top">
+                    <div className="footer-line"></div>
+                    <div className="footer-title">ABOUT</div>
+                    <div className="footer-line"></div>
+                    <div className="footer-line"></div>
+                    <div className="footer-title">CONTACT</div>
+                    <div className="footer-line"></div>
+                    <div className="footer-line"></div>
+                    <div className="footer-title">SOCIAL</div>
+                    <div className="footer-line"></div>
+                </div>
+                <div className="footer-content">
+                    <div className="about-footer">
+                        <p>My Reads: A book tracking app that helps you categorize your books and find new books based on React Nanodegree Program.</p>
+                    </div>
+                    <div className="contact-footer">
+                        <div className="link-item">
+                            <div className="footer-icon linkedin-icon"></div>
+                            <a href="https://www.linkedin.com/in/rabahzeineddine" target="_blank" className="footer-link">Rabah Zeineddine</a>
+                        </div>
+                        <div className="link-item">
+                            <div className="footer-icon mail-icon"></div>
+                            <a href="mailto:rabah.zeineddine@gmail.com" target="_blank" className="footer-link">rabah.zeineddine@gmail.com</a>
+                        </div>
+                    </div>
+                    <div className="social-footer">
+                        <div className="link-item">
+                            <div className="footer-icon facebook-icon"></div>
+                            <a href="https://www.facebook.com/rabah.zeineldine.9" target="_blank" className="footer-link">Rabah Zeineddine</a>
+                        </div>
+                        <div className="link-item">
+                            <div className="footer-icon instagram-icon"></div>
+                            <a href="https://www.instagram.com/rabahzeineddine/" target="_blank" className="footer-link">rabahzeineddine</a>
+                        </div>
+                    </div>
+                </div>
+            </div>
         )
     }
 
